f849c89d221790f18ef9dca5089cac1d26c40674
Kantha Liyanage <kanthal@outlook.com>
2022-03-21 00:35:39 +0530

    Consul skeleton

---
+++ utils/serviceregister/ConsulServiceRegister.cs
@@ -0,0 +1,23 @@
+namespace dc.assignment.primenumbers.utils.serviceregister
+{
+    class ConsulServiceRegister
+    {
+
+        public void setLeader() { }
+
+        public void getLeader() { }
+
+        public void removeLeader() { }
+
+        public void setNode() { }
+        public void getAllNodes() { }
+
+        public void getProposers() { }
+
+        public void getAcceptors() { }
+
+        public void getLearner() { }
+
+    }
+
+}
